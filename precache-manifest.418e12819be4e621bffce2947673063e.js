42e193a3a937ab0ddbf86ef4dcff6a90a44de6a3
healthypackrat <healthypackrat@gmail.com>
2019-10-19 15:43:10 +0900

    Updates

---
+++ precache-manifest.418e12819be4e621bffce2947673063e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93d9e5a992f96e9f542c318609502356",
+    "url": "/todo-app-typescript/index.html"
+  },
+  {
+    "revision": "d1c5b35384ce81843ca5",
+    "url": "/todo-app-typescript/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "90ab0ea7c60ffcf60dd3",
+    "url": "/todo-app-typescript/static/js/2.b2751579.chunk.js"
+  },
+  {
+    "revision": "d1c5b35384ce81843ca5",
+    "url": "/todo-app-typescript/static/js/main.cc477b66.chunk.js"
+  },
+  {
+    "revision": "310151ae887eac8e8d23",
+    "url": "/todo-app-typescript/static/js/runtime-main.6b3bbdfa.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/todo-app-typescript/precache-manifest.418e12819be4e621bffce2947673063e.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/todo-app-typescript/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
